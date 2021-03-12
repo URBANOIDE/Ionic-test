f0eeb32d1f94709aae54fcd4e5fcaa1e23ae48e9
Alberto Albeiro Chindoy Urbano <Albertochind04@gmail.com>
2021-03-12 15:56:06 -0500

    CRUD ionic (taller)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { NavController, AlertController } from '@ionic/angular'
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
   nombre = null;
@@ -14,15 +15,81 @@ export class HomePage {
 
   usuarios = [];
 
-  constructor() {}
+  constructor(public alertController: AlertController) {}
+
 
   guardarDatos(){
     //agregando datos en memoria
     this.usuarios.push({
-      nombre: this.nombre
+      nombre: this.nombre,
+      apellido: this.apellido,
+      celular: this.celular,
+      email: this.email
     })
     //limpiando datos
-    this.nombre=""
+    this.nombre="",
+    this.apellido="",
+    this.celular="",
+    this.email=""
   }
+  
+  eliminarDatos(i){
+    this.usuarios.splice(i, 1);
+  }
+
+  async editarDatos(i) {
+    //crear una ventana alert, para pedir los nuevos registros de la persona que selecciono
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'EDITAR',
+      inputs: [
+        {
+          name: 'name1',
+          type: 'text',
+          value: this.usuarios[i].nombre
+        },
+        {
+          name: 'apellido1',
+          type: 'text',
+          value: this.usuarios[i].apellido
+        },
+        {
+          name: 'celular1',
+          type: 'text',
+          value: this.usuarios[i].celular
+        },
+        {
+          name: 'email1',
+          type: 'text',
+          value: this.usuarios[i].email
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, 
+        { text: 'Editar', handler: data => {
+          //console.log(this.usuarios[i]);
+          
+          this.usuarios[i].nombre = data.name1;
+          this.usuarios[i].apellido = data.apellido1;
+          this.usuarios[i].celular = data.celular1;
+          this.usuarios[i].email = data.email1;  
+          
+        }
+        }
+        
+      ]
+    });
+
+    await alert.present();
+  }
+
+
 
 }
